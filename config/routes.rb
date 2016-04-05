7b2590b164d01583796af7b34357e0425872b6d0
Ha Huy <tongnguyen.hahuy@gmail.com>
2016-04-05 14:00:03 +0700

    paypal api

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Spree::Core::Engine.routes.draw do
   namespace :api do
     resources :orders
     post "orders/:id/cancel" => "/spree/api/orders#cancell"
+    post '/paypal/:order_number', :to => "/spree/paypal#express", :as => :paypal_express
+    get '/paypal/confirm', :to => "/spree/paypal#confirm", :as => :confirm_paypal
+    get '/paypal/cancel', :to => "/spree/paypal#cancel", :as => :cancel_paypal
+    get '/paypal/notify', :to => "/spree/paypal#notify", :as => :notify_paypal
   end
 end
