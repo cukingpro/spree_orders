9971819704162042f64adc6ca72fbfe7b7110ab6
Ha Huy <tongnguyen.hahuy@gmail.com>
2016-04-08 09:42:29 +0700

    fix paypal

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Spree::Core::Engine.routes.draw do
   namespace :api do
     resources :orders
     post "orders/:id/cancel" => "/spree/api/orders#cancell"
-    post '/paypal/:order_number', :to => "/spree/paypal#express", :as => :paypal_express
-    get '/paypal/confirm', :to => "/spree/paypal#confirm", :as => :confirm_paypal
-    get '/paypal/cancel', :to => "/spree/paypal#cancel", :as => :cancel_paypal
-    get '/paypal/notify', :to => "/spree/paypal#notify", :as => :notify_paypal
+    post '/paypal/:order_number', :to => "/spree/api/paypal#express", :as => :paypal_express
+    get '/paypal/confirm', :to => "/spree/api/paypal#confirm", :as => :confirm_paypal
+    get '/paypal/cancel', :to => "/spree/api/paypal#cancel", :as => :cancel_paypal
+    get '/paypal/notify', :to => "/spree/api/paypal#notify", :as => :notify_paypal
+
+    post 'paypall/add_fund', :to => "/spree/api/paypal#add_fund", :as => :add_fund_paypal
+    get 'paypal/confirm_add_fund', :to => "/spree/api/paypal#confirm_add_fund", :as => :confirm_add_fund_paypal
+    get 'paypal/cancel_add_fund', :to => "/spree/api/paypal#cancel_add_fund", :as => :cancel_add_fund_paypal
 
     get '/user_history_orders/', :to => "/spree/api/orders#user_history_orders"
     get '/user_next_orders/', :to => "/spree/api/orders#user_next_orders"
