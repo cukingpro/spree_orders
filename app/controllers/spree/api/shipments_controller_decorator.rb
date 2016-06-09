cf4ebe27fe1c0a791d813b10390397616acf8286
Ha Huy <tongnguyen.hahuy@gmail.com>
2016-06-09 09:51:37 +0700

    save shipment after split

--- app/controllers/spree/api/shipments_controller_decorator.rb
+++ app/controllers/spree/api/shipments_controller_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Api::ShipmentsController.class_eval do
-	
+
   def split
     order = Spree::Order.find_by!(number: params[:order_number])
     original_shipment = Spree::Shipment.friendly.find(params[:original_shipment_number])
@@ -8,17 +8,16 @@ Spree::Api::ShipmentsController.class_eval do
 
     shipments = params[:shipments]
     shipments.each do |sm|
-    	shipment = order.shipments.create(stock_location_id: 1, date_delivery: sm[:date_delivery].to_date, time_frame_id: sm[:time_frame_id].to_i)
+      shipment = order.shipments.create(stock_location_id: 1, date_delivery: sm[:date_delivery].to_date, time_frame_id: sm[:time_frame_id].to_i)
 
-    	line_items = sm[:line_items]
-    	line_items.each do |li|
-    		variant = Spree::Variant.find(li[:variant_id].to_i)
-    		quantity = li[:quantity].to_i
-    		# order.contents.add(variant, quantity, {shipment: shipment})
-    		original_shipment.transfer_to_shipment(variant, quantity, shipment)
-            shipment.address = original_shipment.address
-
-    	end
+      line_items = sm[:line_items]
+      line_items.each do |li|
+        variant = Spree::Variant.find(li[:variant_id].to_i)
+        quantity = li[:quantity].to_i
+        original_shipment.transfer_to_shipment(variant, quantity, shipment)
+        shipment.address = original_shipment.address
+        shipment.save
+      end
     end
     original_shipment.destroy!
     render json: {success: true, message: Spree.t(:shipment_transfer_success)}, status: 201
@@ -39,6 +38,6 @@ Spree::Api::ShipmentsController.class_eval do
     render "spree/api/shipments/index_mobile"
   end
 
-  
+
 
 end
